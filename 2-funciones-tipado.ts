780974389c1afc5428ca8ae42638591219871bc2
NatZigaran. <natanaelzigaran@gmail.com>
2025-11-27 14:34:29 -0300

    Update 2-funciones-tipado.ts to have single function call as per requirements

--- 2-funciones-tipado.ts
+++ 2-funciones-tipado.ts
@@ -2,6 +2,4 @@ function updateStock(currentStock: number, quantityChange: number): number {
     return currentStock + quantityChange;
 }
 
-console.log("Updated stock (10 + 5):", updateStock(10, 5));
-console.log("Updated stock (20 + (-3)):", updateStock(20, -3));
-console.log("Updated stock (0 + 100):", updateStock(0, 100));
+console.log("Updated stock:", updateStock(10, 5));
